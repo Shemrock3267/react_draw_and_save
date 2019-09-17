f8f6bf6a1d21f971d4c16aa7498a05449f79cccf
Roman Oliinyk <oliinyk@oliinyk.local>
2019-09-17 21:59:54 +0300

    added close button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from "react";
+import Popup from "reactjs-popup";
+import SignaturePad from "react-signature-canvas";
 import "./App.css";
+import "./signature.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Draw & save App</h1>
+      <Popup
+        modal
+        trigger={<button> Open drawing pad</button>}
+        closeOnDocumentClick={false}
+      >
+        {close => (
+          <>
+            <SignaturePad
+              canvasProps={{
+                className: "signatureCanvas"
+              }}
+            />
+            <button className="close-btn" onClick={close}>
+              Close
+            </button>
+          </>
+        )}
+      </Popup>
+    </div>
+  );
 }
 
 export default App;
