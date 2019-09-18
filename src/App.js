e51b512a2319b77ed85fa92c27716a2ef90b6cb8
Roman Oliinyk <oliinyk@oliinyk.local>
2019-09-18 09:35:55 +0300

    added clear and save buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Popup from "reactjs-popup";
 import SignaturePad from "react-signature-canvas";
 import "./App.css";
 import "./signature.css";
 
 function App() {
+  const [imageURL, setImageURL] = useState(null);
+  const sigCanvas = useRef({});
+  const clear = () => sigCanvas.current.clear();
+  const save = () =>
+    setImageURL(sigCanvas.current.getTrimmedCanvas().toDataURL("image/png"));
+
   return (
     <div className="App">
-      <h1>Draw & save App</h1>
+      <h1>Draw and save App</h1>
       <Popup
         modal
         trigger={<button> Open drawing pad</button>}
@@ -16,16 +22,33 @@ function App() {
         {close => (
           <>
             <SignaturePad
+              ref={sigCanvas}
               canvasProps={{
                 className: "signatureCanvas"
               }}
             />
+            <button onClick={save}>Save</button>
+            <button onClick={clear}>Clear</button>
             <button className="close-btn" onClick={close}>
               Close
             </button>
           </>
         )}
       </Popup>
+      <br />
+      <br />
+      {imageURL ? (
+        <img
+          src={imageURL}
+          alt="signature"
+          style={{
+            display: "block",
+            margin: "0 auto",
+            border: "1px solid black",
+            width: "250px"
+          }}
+        />
+      ) : null}
     </div>
   );
 }
