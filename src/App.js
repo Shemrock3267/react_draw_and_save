2f65cb8eac2b1934398f8ab85bcfa083ef7d4719
Roman Oliinyk <oliinyk@oliinyk.local>
2019-09-26 18:10:57 +0300

    added bootstrap localy and styled buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useRef } from "react";
 import Popup from "reactjs-popup";
 import SignaturePad from "react-signature-canvas";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./signature.css";
 
 function App() {
@@ -16,23 +17,27 @@ function App() {
       <h1>Draw and save App</h1>
       <Popup
         modal
-        trigger={<button> Open drawing pad</button>}
+        trigger={<button className="btn btn-primary"> Open drawing pad</button>}
         closeOnDocumentClick={false}
       >
         {close => (
-          <>
+          <React.Fragment>
             <SignaturePad
               ref={sigCanvas}
               canvasProps={{
                 className: "signatureCanvas"
               }}
             />
-            <button onClick={save}>Save</button>
-            <button onClick={clear}>Clear</button>
-            <button className="close-btn" onClick={close}>
+            <button className="btn btn-success mr-1 shadow-none" onClick={save}>
+              Save
+            </button>
+            <button className="btn btn-danger shadow-none" onClick={clear}>
+              Clear
+            </button>
+            <button className="btn btn-dark ml-1 shadow-none" onClick={close}>
               Close
             </button>
-          </>
+          </React.Fragment>
         )}
       </Popup>
       <br />
